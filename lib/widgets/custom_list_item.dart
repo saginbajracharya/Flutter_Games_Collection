eca10e1937f6ab86805ca84795cb4f9f32f79d26
sagin bajracharya <saginbajracharya09@gmail.com>
2024-08-05 23:25:03 +0545

    fix

--- lib/widgets/custom_list_item.dart
+++ lib/widgets/custom_list_item.dart
@@ -15,40 +15,16 @@ class CustomListItem extends StatefulWidget {
 class _CustomListItemState extends State<CustomListItem> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: widget.onTap,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const SizedBox(),
-                  Text(widget.trailingText,style: textSmallWhite(),textAlign: TextAlign.start),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(width: 20),
-                  Text(widget.titleText,style: textSmallWhite(),textAlign: TextAlign.start),
-                ],
-              ),
-            )
-          ],
+    return Center(
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Container(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width*0.16 
+          ),
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.all(10.0),
+          child: Text("${widget.trailingText}   ${widget.titleText}",style: textSmallWhite(),textAlign: TextAlign.start),
         ),
       ),
     );
