6ef5fb5c435a2df1331e7518b10b9e50f4fe53c9
sagin bajracharya <saginbajracharya09@gmail.com>
2024-10-17 11:02:34 +0545

    fix

--- lib/game_list_page.dart
+++ lib/game_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_games_collection/games/charactermovement.dart';
 import 'package:flutter_games_collection/games/epictd.dart';
@@ -8,6 +10,11 @@ import 'package:flutter_games_collection/games/emberquest.dart';
 import 'package:flutter_games_collection/games/spaceshooter.dart';
 import 'package:flutter_games_collection/widgets/base_scaffold_layout.dart';
 import 'package:flutter_games_collection/widgets/custom_list_item.dart';
+import 'package:ultimate_particle_fx/particles_enum/movement_direction.dart';
+import 'package:ultimate_particle_fx/particles_enum/particle_shapes.dart';
+import 'package:ultimate_particle_fx/particles_enum/spawn_position.dart';
+import 'package:ultimate_particle_fx/particles_enum/touch_type.dart';
+import 'package:ultimate_particle_fx/ultimate_particle_fx.dart';
 import 'common/styles.dart';
 
 // All List of Games
@@ -35,64 +42,114 @@ class _GamesListPageState extends State<GamesListPage> {
           )
         ],
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // Title
-          Text('title'.tr,style: textLargeWhite()),
-          const SizedBox(height: 20),
-          Text('subTitle'.tr,style: textLargeWhite()),
-          const SizedBox(height: 50),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Space Shooter
-              CustomListItem(
-                trailingText: '1', 
-                titleText: 'game1Title'.tr, 
-                onTap: () async{  
-                  Get.to(()=>const SpaceShooterMenuPage());
-                },
-              ),
-              // Ember Quest
-              CustomListItem(
-                trailingText: '2', 
-                titleText: 'game2Title'.tr, 
-                onTap: () async{  
-                  Get.to(()=>const EmberQuestMenuPage());
-                },
-              ),
-              // Epic TD
-              CustomListItem(
-                trailingText: '3', 
-                titleText: 'game3Title'.tr, 
-                onTap: () async{  
-                  Get.to(()=>const EpicTdMenuPage());
-                },
-              ),
-              // Push Puzzle
-              CustomListItem(
-                trailingText: '4', 
-                titleText: 'game4Title'.tr, 
-                onTap: () async{  
-                  Get.to(()=>const PushPuzzleMenuPage());
-                },
-              ),
-              // Character Movement
-              CustomListItem(
-                trailingText: '5', 
-                titleText: 'game5Title'.tr, 
-                onTap: () async{  
-                  Get.to(()=>const CharacterMovementMenuPage());
-                },
-              )
-            ],
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height-60,
+        child: UltimateParticleFx(
+          neverEnding: true,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          velocity: const Offset(0, 0),
+          position: const Offset(0, 0),
+          colors : const [Colors.green,Colors.yellow,Colors.red,Colors.blue],
+          maxSize: 10.0,
+          minSize: 5.0,
+          lifespan: 2000,
+          maxParticles: 100,
+          speed: 0.4,
+          rotation: 0,
+          shapes: const [
+            ParticleShape.circle, 
+            ParticleShape.square, 
+            ParticleShape.triangle,
+            ParticleShape.star,
+            ParticleShape.hexagon,
+            ParticleShape.diamond,
+            ParticleShape.pentagon,
+            ParticleShape.ellipse,
+            ParticleShape.cross,
+            ParticleShape.heart,
+            ParticleShape.octagon,
+          ],
+          gradient: const LinearGradient(
+            colors: [Colors.red,Colors.blue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.0, 1.0]
           ),
-        ],
+          allowParticlesExitSpawnArea : false,
+          spawnAreaPosition : const Offset(0, 0),
+          spawnPosition : SpawnPosition.random,
+          movementDirection : MovementDirection.random,
+          spawnAreaWidth: MediaQuery.of(context).size.width,
+          spawnAreaHeight: MediaQuery.of(context).size.height,
+          spawnAreaColor : Colors.transparent,
+          touchType: TouchType.push,
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 12.0, sigmaY: 12.0),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Title
+                  Text('title'.tr,style: textLargeWhite()),
+                  const SizedBox(height: 20),
+                  Text('subTitle'.tr,style: textLargeWhite()),
+                  const SizedBox(height: 50),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Space Shooter
+                      CustomListItem(
+                        trailingText: '1', 
+                        titleText: 'game1Title'.tr, 
+                        onTap: () async{  
+                          Get.to(()=>const SpaceShooterMenuPage());
+                        },
+                      ),
+                      // Ember Quest
+                      CustomListItem(
+                        trailingText: '2', 
+                        titleText: 'game2Title'.tr, 
+                        onTap: () async{  
+                          Get.to(()=>const EmberQuestMenuPage());
+                        },
+                      ),
+                      // Epic TD
+                      CustomListItem(
+                        trailingText: '3', 
+                        titleText: 'game3Title'.tr, 
+                        onTap: () async{  
+                          Get.to(()=>const EpicTdMenuPage());
+                        },
+                      ),
+                      // Push Puzzle
+                      CustomListItem(
+                        trailingText: '4', 
+                        titleText: 'game4Title'.tr, 
+                        onTap: () async{  
+                          Get.to(()=>const PushPuzzleMenuPage());
+                        },
+                      ),
+                      // Character Movement
+                      CustomListItem(
+                        trailingText: '5', 
+                        titleText: 'game5Title'.tr, 
+                        onTap: () async{  
+                          Get.to(()=>const CharacterMovementMenuPage());
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ) 
     );
   }
--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -25,7 +25,7 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return BaseScaffoldLayout(
-      extendBehindAppBar: true,
+      extendBehindAppBar: false,
       showScrollBar: true,
       appbar: AppBar(
         backgroundColor: transparent,
@@ -36,82 +36,102 @@ class _SettingsPageState extends State<SettingsPage> {
         title: Text('settings'.tr,style: textLargeWhite()),
         centerTitle: true,
       ),
+      bodyContentAlignment: Alignment.topCenter,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
+          // Language dropdown selection
           SizedBox(
             width: double.infinity,
-            child: DropdownButton<String>(
-              isExpanded: true,
-              value: settingsCon.selectedLanguage==""?'en':settingsCon.selectedLanguage,
-              dropdownColor: black,
-              onChanged: (String? newValue) {
-                if (newValue != null) {
-                  setState(() {
-                    settingsCon.selectedLanguage = newValue;
-                  });
-                  write(StorageKeys.selectedLocaleKey,newValue);
-                  var locale = Locale(newValue);
-                  Get.updateLocale(locale);
-                }
-              },
-              items: [
-                DropdownMenuItem<String>(
-                  value: 'en',
-                  child: Text('english'.tr, style: textSmallWhite()),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'jp',
-                  child: Text('japanese'.tr, style: textSmallWhite()),
-                ),
-                DropdownMenuItem<String>(
-                  value: 'np',
-                  child: Text('nepal'.tr, style: textSmallWhite()),
-                ),
-              ],
-              underline: Container(),
-              icon: const Icon(Icons.arrow_drop_down, color: white),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 1.5), // Adjust the color and width as needed
+                borderRadius: BorderRadius.circular(10), // Optional: to give rounded corners
+              ),
+              padding: const EdgeInsets.only(left: 20.0,right:20.0,top: 10.0,bottom: 5.0),
+              child: DropdownButton<String>(
+                isExpanded: true,
+                value: settingsCon.selectedLanguage == "" ? 'en' : settingsCon.selectedLanguage,
+                dropdownColor: black,
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      settingsCon.selectedLanguage = newValue;
+                    });
+                    write(StorageKeys.selectedLocaleKey, newValue);
+                    var locale = Locale(newValue);
+                    Get.updateLocale(locale);
+                  }
+                },
+                items: [
+                  DropdownMenuItem<String>(
+                    value: 'en',
+                    child: Text('english'.tr, style: textSmallWhite()),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'jp',
+                    child: Text('japanese'.tr, style: textSmallWhite()),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'np',
+                    child: Text('nepal'.tr, style: textSmallWhite()),
+                  ),
+                ],
+                underline: Container(), // Remove default underline
+                icon: const Icon(Icons.arrow_drop_down, color: white),
+              ),
             ),
           ),
+          const SizedBox(height: 40.0),
+          // Music Selection
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: settingsCon.isMusicEnabled,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        settingsCon.isMusicEnabled = value ?? false;
-                      });
-                    },
-                    activeColor: green,
-                    checkColor: white,
-                  ),
-                  const SizedBox(width: 8.0),
-                  Text('music'.tr, style: textSmallWhite()),
-                ],
+              SizedBox(
+                width: 40, // Adjust the size based on your need
+                height: 40,
+                child: Checkbox(
+                  value: settingsCon.isMusicEnabled,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      settingsCon.isMusicEnabled = value ?? false;
+                    });
+                  },
+                  activeColor: green,
+                  checkColor: white,
+                ),
               ),
-              Row(
-                children: [
-                  Checkbox(
-                    value: settingsCon.isSoundEnabled,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        settingsCon.isSoundEnabled = value ?? false;
-                      });
-                    },
-                    activeColor: green,
-                    checkColor: white,
-                  ),
-                  const SizedBox(width: 8.0),
-                  Text('sound'.tr, style: textSmallWhite()),
-                ],
-              ),
+              const SizedBox(width: 10.0),
+              Text('music'.tr, style: textSmallWhite()),
             ],
           ),
+          const SizedBox(height: 30.0),
+          // Sound Selection
+          Row(
+            children: [
+              SizedBox(
+                width: 40, // Adjust the size based on your need
+                height: 40,
+                child: Checkbox(
+                  value: settingsCon.isSoundEnabled,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      settingsCon.isSoundEnabled = value ?? false;
+                    });
+                  },
+                  activeColor: green,
+                  checkColor: white,
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Text('sound'.tr, style: textSmallWhite()),
+            ],
+          ),
+          const SizedBox(height: 30.0),
         ],
       ) 
     );
