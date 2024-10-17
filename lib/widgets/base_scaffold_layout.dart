1529df85be72487fccdf21e94f1b23ef63b3d103
sagin bajracharya <saginbajracharya09@gmail.com>
2024-10-17 11:04:49 +0545

    fix widgets

--- lib/widgets/base_scaffold_layout.dart
+++ lib/widgets/base_scaffold_layout.dart
@@ -3,11 +3,12 @@ import 'package:flutter_games_collection/common/styles.dart';
 
 // Base Scaffold Layout for All Pages
 class BaseScaffoldLayout extends StatelessWidget {
-  const BaseScaffoldLayout({super.key,required this.child,this.appbar, this.extendBehindAppBar, this.showScrollBar});
+  const BaseScaffoldLayout({super.key,required this.child,this.appbar, this.extendBehindAppBar, this.showScrollBar, this.bodyContentAlignment});
   final Widget child;
   final AppBar? appbar;
   final bool? extendBehindAppBar;
   final bool? showScrollBar;
+  final Alignment? bodyContentAlignment;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +20,7 @@ class BaseScaffoldLayout extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         padding: const EdgeInsets.all(10),
-        alignment: Alignment.center,
+        alignment: bodyContentAlignment??Alignment.center,
         child: showScrollBar==true
         ?Scrollbar(
           interactive: true,
